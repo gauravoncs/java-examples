79ea397f6abcdf63d1f5be788b96b84d88ee54be
Gaurav Gaur <gaurav.gaur@navis.com>
2023-05-25 18:34:11 +0530

    Hello world and Boxing examples

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # java-examples
 Java examples
+
+## Hello World
+### Code
+Package: com.csr.examples.java.helloworld
+
+## Boxing
+### Code
+Package: com.csr.examples.java.boxing
---
+++ com/csr/examples/java/helloworld/HelloWorld.java
@@ -0,0 +1,7 @@
+package com.csr.examples.java.helloworld;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.err.println("Hello World!");
+    }
+}
